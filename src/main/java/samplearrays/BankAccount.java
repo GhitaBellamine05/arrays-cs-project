fdacfdde3eef3872cc066f169bce6efa4d281fd1
Ghita Bellamine <ghita.bellamine@um6p.ma>
2025-10-04 15:32:17 +0100

    Commit Exercise3

--- src/main/java/samplearrays/BankAccount.java
+++ src/main/java/samplearrays/BankAccount.java
@@ -4,26 +4,58 @@ public class BankAccount {
 
     String name;
     double currentBalance;
-    //TO-DO: Initialize an Array with 1000 in size that stores Double called 'transactions' to keep track of the user's transactions
+    // Initialize an Array with 1000 in size that stores Double called 'transactions' to keep track of the user's transactions
+    double[] transactions = new double[1000];
+    int transactionsCounter = 0;
 
     public BankAccount(String name, int startingBalance){
-
+        this.name =  name;
+        this.currentBalance = startingBalance;
     }
 
     public void deposit(double amount){
+        if(amount<0){
+            System.out.println("Unsuccessful deposit : Invalid amount");
+        }
+        else{
+            transactions[transactionsCounter] = amount;
+            currentBalance += amount;
+            transactionsCounter++;
+            System.out.println("-------------------------------" +
+                    "\nDepositor : "+name +
+                    "\nDeposited Amount : "+amount +
+                    "\nNew balance : "+ currentBalance +
+                    "\n-------------------------------");
+        }
 
     }
 
     public void withdraw(double amount){
-
+        if (amount < 0 || amount > currentBalance){
+            System.out.println("Unsuccessful withdraw : Invalid amount");
+        }
+        else{
+            currentBalance -= amount;
+            transactions[transactionsCounter] = -1 * amount;
+            transactionsCounter++;
+        }
     }
 
     public void displayTransactions(){
+        System.out.println("All transactions : ");
+        for ( double transaction : transactions){
+            if (transaction > 0) {
+                System.out.println("Deposit : " + transaction);
+            }
+            else {
+                System.out.println("Withdraw : " + transaction);
+            }
 
+        }
     }
 
     public void displayBalance(){
-
+        System.out.println("Balance : " + currentBalance);
     }
 
     public static void main(String[] args) {
